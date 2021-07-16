317f3519a29f6eac6f2e5073f726a13b3dfa021c
Aitik Gupta <aitikgupta@gmail.com>
2021-07-16 17:38:41 +0530

    ENH: Initialize global config

---
+++ PayEnd/config.py
@@ -0,0 +1,9 @@
+import os
+
+# Because GenZ loves video games!
+PREDEFINED_QUERY = "fortnite"
+
+# poll interval (in seconds)
+SLEEP_TIME = 10
+
+GLOBAL_API_KEYS = os.getenv("GOOGLE_API_KEYS").split(",")
