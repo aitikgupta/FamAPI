739a9a4e35302de1dd8bf422a6e98f2c4cb92ca0
Aitik Gupta <aitikgupta@gmail.com>
2021-07-17 10:19:18 +0530

    ENH: Stitch everything together

--- FamEnd/src/App.js
+++ FamEnd/src/App.js
@@ -1,20 +1,87 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+
+import Button from './Components/Button';
+import Search from './Components/Search';
+import VideoCard from './Components/VideoCard';
+
+import { fetchLatest } from './service/Fetch';
+
+import "./App.css";
+
 
 function App() {
-  const [message, setMessage] = useState(null);
+  const [result, setResult] = useState([]);
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+
+
+  const getVideos = async (newPage) => {
+    try {
+        const { results } = await fetchLatest(newPage ?? currentPage, query);
+        let videoCards = results.map(element => < VideoCard key = {
+                element._id
+            }
+            title = {
+                element.title
+            }
+            description = {
+                element.description
+            }
+            thumbnailDefaultResolution = {
+                element.thumbnail_defaultres
+            }
+            thumbnailHighResolution = {
+                element.thumbnail_highres
+            }
+            />);
+            console.log("[FamEnd] New results in!");
+            console.log(videoCards);
+            if (videoCards.length > 0) {
+                setResult(videoCards);
+            }
+            else {
+                setResult("No more results..")
+            }
+        }
+        catch (error) {
+            // something went wrong
+            console.log("[FamEnd] Error: ", error);
+        }
+    }
 
   useEffect(() => {
-    fetch('http://localhost:8080')
-        .then(response => response.json())
-        .then(data => setMessage(data.message));
+    // fetch as soon as mounted
+    getVideos();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+
+  const updatePage = (newPage) => {
+      console.log("[FamEnd] Updating page:", newPage);
+      setCurrentPage(newPage);
+      getVideos(newPage);
+  }
+
+  const onClickHandler = () => {
+      updatePage(currentPage + 1);
+  }
+
+  const pagination = <Button text={"Next ->"} onClickHandler={onClickHandler} />
 
   return (
     <div className="App">
-        <header className="App-header">
-            { message ? <p>{message}</p> : <p>Loading...</p> }
-        </header>
+        <header className="App-header"/>
+
+        <Search query={query} setQuery={setQuery} getVideos={getVideos} />
+
+        <div className="videoHolder">
+            {result}
+        </div>
+
+        <div className="paginationHolder">
+            {pagination}
+        </div>
     </div>
   );
 }
